0f67d677fe91ca883564410196c7a3ff7933391e
Gninoo1 <gninoo@dian.gov.co>
2024-05-16 10:50:38 -0500

    Update ApplicationUser.cs

--- src/PartsUnlimited.Models/ApplicationUser.cs
+++ src/PartsUnlimited.Models/ApplicationUser.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// 2024-05-16 Cambio desde Git Hub (Rama Main)
 using Microsoft.AspNetCore.Identity;
 
 namespace PartsUnlimited.Models
